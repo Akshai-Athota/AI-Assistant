e25fdc9fa33351b78167f16fd0d37322ae31cba8
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-09-19 19:44:35 +0530

    Initial commit

---
+++ SoundToTextService/src/main/java/com/akshai/SoundToTextService/SoundToTextServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akshai.SoundToTextService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundToTextServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundToTextServiceApplication.class, args);
+	}
+
+}
---
+++ SoundToTextService/src/test/java/com/akshai/SoundToTextService/SoundToTextServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshai.SoundToTextService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundToTextServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
